eb40cb40173f87af63fe48c5860e9cfdf494f55b
sinan-snk <38116536+sinan-snk@users.noreply.github.com>
2021-04-14 04:44:42 +0300

    Add files via upload

---
+++ sociality.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: localhost:8889
+-- Üretim Zamanı: 14 Nis 2021, 01:25:23
+-- Sunucu sürümü: 5.7.26
+-- PHP Sürümü: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `sociality`
+--
+
+--
+-- Tablo döküm verisi `bot`
+--
+
+INSERT INTO `bot` (`id`, `product`, `images`, `price`) VALUES
+(1, 'Brass or Silver Leaf Bookmark Set\n', 'https://i.etsystatic.com/12149676/r/il/b96248/2959017777/il_794xN.2959017777_t44r.jpg', '£9.50\n                    ');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
